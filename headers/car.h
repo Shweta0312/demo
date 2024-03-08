6d2f9c9f361cca57e5c540b19d3b225126bdfe27
Nihaal <nihaal.shirkar@indiraicem.ac.in>
2024-03-08 11:49:34 +0530

    Added new files

---
+++ headers/car.h
@@ -0,0 +1,14 @@
+#ifndef DEMO_CAR_H
+#define DEMO_CAR_H
+
+#include <string>
+
+class Car{
+public:
+    std::string brand;
+    std::string model;
+    std::string registrationNumber;
+    int price;
+};
+
+#endif //DEMO_CAR_H
---
+++ headers/functions.h
@@ -0,0 +1,14 @@
+#ifndef DEMO_FUNCTIONS_H
+#define DEMO_FUNCTIONS_H
+
+#include <vector>
+#include "car.h"
+
+void addCar(std::vector <Car> &cars);
+void removeCar(std::vector <Car> &carList);
+void manageCar(std::vector <Car> &cars);
+void manageSubscriber();
+void manageSubscription();
+void reports();
+
+#endif //DEMO_FUNCTIONS_H
